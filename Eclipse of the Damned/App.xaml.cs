cc2cf38c2508462632633cb35f3671e624b44b51
axolo <axolo@NIGGASTATIONPRO>
2024-10-11 09:19:39 +0200

    Přidejte soubory projektu.

---
+++ Eclipse of the Damned/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Eclipse_of_the_Damned
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
